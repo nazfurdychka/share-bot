702c5bdb0ece47df0d30d24083d590b5592b4a0e
IvanShkvir <ivanshkvir@gmail.com>
2021-07-19 13:20:46 +0300

    changed start text

--- bot/handlers/users/start.py
+++ bot/handlers/users/start.py
@@ -7,4 +7,4 @@ from bot.utils.misc.decorators import check_if_user_is_registered
 @dp.message_handler(commands="start")
 @check_if_user_is_registered
 async def bot_start(message: types.Message):
-    await message.answer(f"Hello, {message.from_user.full_name}, I'm Sharebot and I can help you to distribute your money when your buy something with somebody.")
+    await message.answer(f"Hello, {message.from_user.full_name}, I'm ShareBot and I will help you manage your shared expenses. Add me to group and enter /help for more info.")
