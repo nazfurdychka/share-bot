10fa603714c36174fc3c5566fd4ee6cca3a261c8
IvanShkvir <ivanshkvir@gmail.com>
2021-07-19 12:03:47 +0300

    added time when group was added, removed list_of_purchases from users

--- bot/utils/db/User.py
+++ bot/utils/db/User.py
@@ -1,7 +1,6 @@
 class User:
-    def __init__(self, user_id: int, cards, username: str, full_name: str, list_of_purchases: list):
+    def __init__(self, user_id: int, cards, username: str, full_name: str):
         self.telegram_id = user_id
         self.cards = cards
         self.username = username
         self.full_name = full_name
-        self.list_of_purchases = list_of_purchases
